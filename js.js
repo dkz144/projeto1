56187e46d54ff8e14ca647644c81f07cf87d2d06
dkzmf144 <dkzmf144@gmail.com>
2025-02-27 14:34:43 -0300

    update js.js

--- js.js
+++ js.js
@@ -2,9 +2,6 @@
 const menuToggle = document.getElementById("menu-toggle");
 const menu = document.getElementById("menu");
 
-// Verifica se os elementos existem para evitar erros no carregamento
-if (menuToggle && menu) {
-    menuToggle.addEventListener("click", function() {
-        menu.classList.toggle("hidden"); // Alterna a classe "hidden"
-    });
-}
+menuToggle.addEventListener("click", function() {
+    menu.classList.toggle("show"); // Alterna a classe "show"
+});
