dfefdd1f4e885b7213fba0ad8c8ef877f17bbb2c
dkzmf144 <dkzmf144@gmail.com>
2025-02-27 14:40:36 -0300

    summary

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ const menuToggle = document.getElementById("menu-toggle");
 const menu = document.getElementById("menu");
 
 menuToggle.addEventListener("click", function() {
-    menu.classList.toggle("show"); // Alterna a classe "show"
+    menu.classList.toggle("show"); // Alterna a classe "show", controlando a visibilidade do menu
 });
